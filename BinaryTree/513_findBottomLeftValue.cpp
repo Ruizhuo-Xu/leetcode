25e073c49ba102e03e819f2de705a372923732f2
ruizhuo <ruizhuoxu@163.com>
2023-12-18 11:12:05 +0800

    binaryTree 12-18-morning

---
+++ BinaryTree/513_findBottomLeftValue.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <string>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        queue<TreeNode*> que;
+        if (root != nullptr) {
+            que.push(root);
+        }
+        string s = "[";
+        while (!que.empty()) {
+            TreeNode* node = que.front();
+            que.pop();
+            if (node != nullptr) {
+                s.append(to_string(node->val));
+                que.push(node->left);
+                que.push(node->right);
+            } else {
+                s.append("null");
+            }
+            s.append(",");
+        }
+        if (s.size() > 1) {
+            s[s.size() - 1] = ']';
+        } else {
+            s += "]";
+        }
+        return s;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        vector<string> record;
+        string s;
+        for (int i = 1; i < data.size(); i++) {
+            if (data[i] != ',' && data[i] != ']') {
+                s += data[i];
+            } else {
+                record.push_back(s);
+                s.clear();
+            }
+        }
+        queue<TreeNode*> que;
+        TreeNode* root = nullptr;
+        if (record[0] == "") {
+            return root;
+        } else {
+            root = new TreeNode(stoi(record[0]));
+            que.push(root);
+        }
+        int i = 1;
+        while (!que.empty() && i < record.size()) {
+            TreeNode* node = que.front();
+            que.pop();
+            if (i < record.size() && record[i] != "null") {
+                node->left = new TreeNode(stoi(record[i]));
+                que.push(node->left);
+            }
+            i++;
+            if (i < record.size() && record[i] != "null") {
+                node->right = new TreeNode(stoi(record[i]));
+                que.push(node->right);
+            }
+            i++;
+        }
+        return root;
+    }
+};
+
+class Solution {
+public:
+    int maxDepth = 0;
+    int findBottomLeftValue(TreeNode* root) {
+
+    }
+
+    int traversal(TreeNode* cur, int& depth, int& res) {
+        depth++;
+        maxDepth = depth > maxDepth ? depth : maxDepth;
+        if (cur->left == nullptr && cur->right == nullptr) {
+            return -1;
+        }
+        if (cur->left) {
+            int left = traversal(cur->left, depth, res);
+            if (depth > maxDepth) {
+                maxDepth = depth;
+                if (left == -1) {
+                    res = cur->left->val;
+                }
+            }
+            depth--;
+        }
+        if (cur->right) {
+            int right = traversal(cur->right, depth, res);
+            if (depth > maxDepth) {
+                maxDepth = depth;
+            }
+            depth--;
+        }
+    }
+};
+
+int main() {
+    string data = "[1,2,3,4,null,5,6,null,null,7]";
+    TreeNode* root = Codec().deserialize(data);
+    cout << Solution().findBottomLeftValue(root) << endl;
+    return 0;
+}
